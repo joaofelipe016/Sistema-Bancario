95d8bd654a889c8bede150d24cf3a9fcb3940e64
joaofelipe016 <61212164+joaofelipe016@users.noreply.github.com>
2021-11-23 09:32:54 -0300

    codigo inicial

---
+++ Sistema-Bancario/src/application/Banco.java
@@ -0,0 +1,22 @@
+package application;
+
+import backgroud.Correntista;
+
+public class Banco {
+
+	public static void main(String[] main) {
+		
+		Correntista myObjt1 = new Correntista("José"); 
+		Correntista myObjt2 = new Correntista("Ana"); 
+		Correntista myObjt3 = new Correntista("Fernanda"); 
+		Correntista myObjt4 = new Correntista("Marcos"); 
+		Correntista myObjt5 = new Correntista("Livia"); 
+	
+		System.out.println("Codigo: " + myObjt1.getCodigo() + " Nome: " + myObjt1.getNome());
+		System.out.println("Codigo: " + myObjt2.getCodigo() + " Nome: " + myObjt2.getNome());
+		System.out.println("Codigo: " + myObjt3.getCodigo() + " Nome: " + myObjt3.getNome());
+		System.out.println("Codigo: " + myObjt4.getCodigo() + " Nome: " + myObjt4.getNome());
+		System.out.println("Codigo: " + myObjt5.getCodigo() + " Nome: " + myObjt5.getNome());
+	}
+	
+}
