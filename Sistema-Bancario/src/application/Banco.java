1529010fca7323a7bd159f8a01603c39c3fdadc1
joaofelipe016 <61212164+joaofelipe016@users.noreply.github.com>
2021-11-26 09:49:40 -0300

    Criado a classe para cadastro

--- Sistema-Bancario/src/application/Banco.java
+++ Sistema-Bancario/src/application/Banco.java
@@ -1,133 +1,53 @@
 package application;
 
 import java.util.Scanner;
-import Correntista.CorrentistaFisico;
-import Correntista.CorrentistaVip;
-import Correntista.CorrentistaJuridico;
+
+import Cadastro.Cadastro;
 
 public class Banco {
 
-		public static void Fisico() {
-			
-			Scanner sc1 = new Scanner(System.in);
-			
-			System.out.print("Nome: ");
-			
-			String nome = sc1.next();
-			
-			Scanner sc2 = new Scanner(System.in);
-			
-			System.out.print("Numero telefonico: ");
-			
-			int numero = sc2.nextInt();
-			
-			Scanner sc3 = new Scanner(System.in);
-			
-			System.out.print("CPF: ");
-			
-			int cpf = sc3.nextInt();
-			
-			CorrentistaFisico fisico = new CorrentistaFisico(nome, numero, cpf);
-			
-			System.out.println("Codigo: " + fisico.getCodigo() + " Nome: " + fisico.getNome() + " numero telefonico: " + fisico.getNumero());
-			
-		}
-		
-		public static void Vip() {
-				
-			Scanner sc1 = new Scanner(System.in);
-			
-			System.out.print("Nome: ");
-			
-			String nome = sc1.next();
-			
-			Scanner sc2 = new Scanner(System.in);
-			
-			System.out.print("Numero telefonico: ");
-			
-			int numero = sc2.nextInt();
-	
-			Scanner sc3 = new Scanner(System.in);
-			
-			System.out.print("CPF: ");
-			
-			int cpf = sc3.nextInt();
-			
-			CorrentistaVip vip = new CorrentistaVip(nome, numero, cpf); 
-			
-			System.out.println("Codigo: " + vip.getCodigoVip() + " Nome: " + vip.getNome() + " numero telefonico: " + vip.getNumero() + " CPF: " + vip.getCpf());
-			
-		}
-		
-		public static void Juridico() {
-			
-			Scanner sc1 = new Scanner(System.in);
-			
-			System.out.print("Nome: ");
-			
-			String nome = sc1.next();
-			
-			Scanner sc2 = new Scanner(System.in);
-			
-			System.out.print("Numero telefonico: ");
-			
-			int numero = sc2.nextInt();
-			
-			Scanner sc3 = new Scanner(System.in);
-			
-			System.out.print("Nome da empresa: ");
-			
-			String nomeEmpresa = sc3.next();
-			
-			Scanner sc4 = new Scanner(System.in);
-			
-			System.out.print("CPF: ");
-			
-			int cpf = sc4.nextInt();
-			
-			CorrentistaJuridico juridico = new CorrentistaJuridico(nome, numero, nomeEmpresa, cpf); 
-			
-			System.out.println("Codigo: " + juridico.getCodigoJuridico() + " Nome: " + juridico.getNome() + " numero telefonico: " + juridico.getNumero() + " empresa: " + juridico.getNomeEmpresa());
-			
-		}
+	static Cadastro cd = new Cadastro();
 	
 	public static void main(String[] main) {
 		
-		boolean x = true;
+		while(true) {
 			
-		while ( x == true) {
-			
-			Scanner sc = new Scanner(System.in);
-			
-			System.out.println("Escolha a conta de sua preferencia: ");	
-			
-			String resp = sc.next();
-			
-			switch (resp) {
-			
-			case "F": 
+			boolean x = true;
 				
-				Fisico();
-				x = false;
-				break;
-			
-			case "V":
-			
-				Vip();
-				x = false;
-				break;
+			while ( x == true) {
 				
-			case "J":
-			
-				Juridico();
-				x = false;
-				break;
-			
-			default:
-			
-				System.out.println("ERRO. Tipo de conta inexistente, tente novamente");
+				Scanner sc = new Scanner(System.in);
 				
-				break;
+				System.out.println("Escolha a conta de sua preferencia: ");	
+				
+				String resp = sc.next();
+				
+				switch (resp) {
+				
+				case "F": 
+					
+					Cadastro.Fisico();
+					x = false;
+					break;
+				
+				case "V":
+				
+					Cadastro.Vip();
+					x = false;
+					break;
+					
+				case "J":
+				
+					Cadastro.Juridico();
+					x = false;
+					break;
+				
+				default:
+				
+					System.out.println("ERRO. Tipo de conta inexistente, tente novamente");
+					
+					break;
+				}
 			}
 		}
 	}
